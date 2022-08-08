98e5852172cc1d121d2b97da0637c998b34edba3
Anton Metlitsky <ticket1201@gmail.com>
2022-08-08 18:57:06 +0300

    feat: add api key, implement API axios methods

--- src/lessons/lesson3/API.ts
+++ src/lessons/lesson3/API.ts
@@ -1,15 +1,17 @@
 import axios from 'axios';
 
 const configOMB = {
-    baseURL: 'http://www.omdbapi.com',
+    baseURL: 'http://www.omdbapi.com/',
 };
-const key = '';
+const key = 'f7c9d2f2';
 const axiosInstance = axios.create(configOMB);
 
 const API = {
     searchFilmsByTitle: (title: string) => {
+       return  axiosInstance.get(`?apikey=${key}&s=${title}`).then((data)=> data.data.Search).catch(()=> `no movies found`)
     },
     searchFilmsByType: (title: string, type: string) => {
+        return  axiosInstance.get(`?apikey=${key}&s=${title}&type=${type}`).then((data)=> data.data.Search).catch(()=> `not found`)
     }
 };
 
